881c27f0772886d30bae21859c71ac29bc6fce43
Your Name <you@example.com>
2022-04-12 19:08:09 +0200

    commit Malik01

--- ConsoleApp_lab3/ConsoleApp_lab3/Program.cs
+++ ConsoleApp_lab3/ConsoleApp_lab3/Program.cs
@@ -5,12 +5,65 @@ namespace ConsoleApp1
 {
     class Program
     {
+        static int m, n, p, q, i, j;
+        static int[,] a, b;
+
+        static void loadParameters()
+        {
+            Console.WriteLine("\nPodaj rozmiar m macierzy A: ");
+            m = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("\nPodaj rozmiar n macierzy A: ");
+            n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("\nPodaj rozmiar p macierzy B: ");
+            p = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("\nPodaj rozmiar q macierzy B: ");
+            q = int.Parse(Console.ReadLine());
+
+            if (n != p)
+            {
+                Console.Write("Mnozenie macierzy niemozliwe. Podaj poprawne wymiary\n");
+                loadParameters();
+            }
+            else
+            {
+                a = new int[m, n];
+                b = new int[p, q];
+
+                for (i = 0; i < m; i++)
+                {
+                    for (j = 0; j < n; j++)
+                    {
+                        a[i, j] = j + i*n;    // elementy macierzy = kolejne liczby naturalne + 0
+                    }
+                }
+
+                for (i = 0; i < p; i++)
+                {
+                    for (j = 0; j < q; j++)
+                    {
+                        b[i, j] = j + i * n;    // elementy macierzy = kolejne liczby naturalne + 0
+                    }
+                }
+
+            }
+        }
+
+
         static void Main(string[] args)
         {
-            int m = 2, n = 3, p = 3, q = 3, i, j;
-            int[,] a = { { 1, 4, 2 }, { 2, 5, 1 } };
-            int[,] b = { { 3, 4, 2 }, { 3, 5, 7 }, { 1, 2, 1 } };
-            Console.WriteLine("Matrix a:");
+            //int m = 2, n = 3, p = 3, q = 3, i, j;
+            //int[,] a = { { 1, 4, 2 }, { 2, 5, 1 } };
+            //[,] b = { { 3, 4, 2 }, { 3, 5, 7 }, { 1, 2, 1 } };
+
+            loadParameters();
+
+
+
+
+            Console.WriteLine("\nMatrix A:");
             for (i = 0; i < m; i++)
             {
                 for (j = 0; j < n; j++)
@@ -19,7 +72,8 @@ namespace ConsoleApp1
                 }
                 Console.WriteLine();
             }
-            Console.WriteLine("Matrix b:");
+
+            Console.WriteLine("\nMatrix B:");
             for (i = 0; i < p; i++)
             {
                 for (j = 0; j < q; j++)
@@ -28,39 +82,32 @@ namespace ConsoleApp1
                 }
                 Console.WriteLine();
             }
-            if (n != p)
+
+
+            Test test = new Test();
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+            Thread[] threads = new Thread[m];
+
+            for (i = 0; i < m; i++)
             {
-                Console.Write("Mnozenie macierzy niemozliwe");
-
+                var temp = i;
+                threads[i] = new Thread(() => test.multiplicate(a, b, q, m, n, i));
             }
-            else
+            for (i = 0; i < m; i++)
             {
-                Test test = new Test();
-                var watch = System.Diagnostics.Stopwatch.StartNew();
-                Thread[] threads = new Thread[m];
-
-                for (i = 0; i < m; i++)
-                {
-                    var temp = i;
-                    threads[i] = new Thread(() => test.multiplicate(a, b, q, m, n, i));
-                }
-                for (i = 0; i < m; i++)
-                {
-                    threads[i].Start();
-                }
-                for (i = 0; i < m; i++)
-                    threads[i].Join();
-                watch.Stop();
-                var elapsedMs = watch.ElapsedMilliseconds;
-                Console.WriteLine(elapsedMs + " ms");
-
+                threads[i].Start();
             }
+            for (i = 0; i < m; i++)
+                threads[i].Join();
+            watch.Stop();
+            var elapsedMs = watch.ElapsedMilliseconds;
+            Console.WriteLine(elapsedMs + " ms");
+
+
 
         }
         public class Test
         {
-
-
 
             public void multiplicate(int[,] A, int[,] B, int q, int m, int n, int j)
             {
